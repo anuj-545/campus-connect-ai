d10b847c2e5856c4d8f25172d14621ee59192161
anuj-545 <134717616+anuj-545@users.noreply.github.com>
2026-07-13 00:59:09 +0530

    Create reusable Input component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,8 @@ import Hero from"./components/Hero/Hero";
 import "./App.css";
 import FeatureCard from "./components/FeatureCard/FeatureCard";
 import Features from "./components/Features/Features";
-import Counter from "./components/Counter/Counter";
-import Nameform from "./components/Nameform/Nameform";
-import SubmitForm from "./components/SubmitForm/SubmitForm";
+import Input from "./components/Input/Input";
+
 
 function App(){
   return(
@@ -13,9 +12,7 @@ function App(){
       <Navbar/>
       <Hero/>
       <Features/>
-      <Counter/>
-      <Nameform/>
-      <SubmitForm/>
+      <Input/>
     </>
   );
 }
---
+++ frontend/src/components/Input/Input.jsx
@@ -0,0 +1,23 @@
+
+
+function Input({ label, type, name, placeholder, value, onChange, error}){
+    return(
+        <div className="input-group">
+            <label htmlFor={name}>{label}</label>
+
+            <input
+                id={name}
+                type={type}
+                name={name}
+                placeholder={placeholder}
+                value={value}
+                onChange={onChange}
+            />
+
+            {error && <p className = "error-message">{error}</p>}
+        </div>
+    );
+
+}
+
+export default Input;
